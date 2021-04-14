9be6d40e5cc30329fa118b5cccf36b0c82fa4a59
ryan2414 <75060346+ryan2414@users.noreply.github.com>
2021-04-14 16:20:05 +0900

    Image 교체

--- Assets/SJ/Scripts/ARTrackedImg.cs
+++ Assets/SJ/Scripts/ARTrackedImg.cs
@@ -11,8 +11,10 @@ public class ARTrackedImg : MonoBehaviour
 
     public ARTrackedImageManager trackedImageManager;
 
+    //게임 오브젝트를 받아와서 이름과 오브젝트를 저장 하기 위한 변수
     public List<GameObject> _objectList = new List<GameObject>();
     private Dictionary<string, GameObject> _prefabDic = new Dictionary<string, GameObject>();
+
     //시간이 되면 사라지게 하기 위한 변수 선언
     private List<ARTrackedImage> _trackedImg = new List<ARTrackedImage>();
     private List<float> _trackedTimer = new List<float>();
@@ -32,17 +34,21 @@ public class ARTrackedImg : MonoBehaviour
         {
             List<ARTrackedImage> tNumList = new List<ARTrackedImage>();
 
+            //trackedImage를 검색해서
             for (int i = 0; i < _trackedImg.Count; i++)
             {
-                if(_trackedImg[i].trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Limited)
+                //TrackingState.Limited == Some tracking information is available, but it is limited or of poor quality.
+                //trackedImage의 상태가 Limited가 된다면
+                if (_trackedImg[i].trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Limited)
                 {
+                    //타이머를 돌려서 타이머 만큼의 시간이 되면
                     if(_trackedTimer[i] > _timer)
                     {
-                        //Remove
+                        //limied된 게임 오브젝트를 setactive(false)로 해준다.
                         string name = _trackedImg[i].referenceImage.name;
                         GameObject tObj = _prefabDic[name];
                         tObj.SetActive(false);
-                        tNumList.Add(_trackedImg[i]);
+                        tNumList.Add(_trackedImg[i]);//setactive(false)된 게임오브젝트를 _trackedImg, _trackedTimer리스트에서 제거하기 위한 리스트에 저장
                     }
                     else
                     {
