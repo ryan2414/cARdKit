d9190b0b420c2c22a8a888f873560d17310e8f03
ryan2414 <75060346+ryan2414@users.noreply.github.com>
2021-04-27 15:14:14 +0900

    ARScene Tutoreal 제작 승형이 texturerenderer 사용하여 3d로 인식 텍스트 상자 만들기 애니메이션 효과 추가

---
+++ Assets/SJ/Scripts/ARScene/SSH_Text.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SSH_Text : MonoBehaviour
+{
+    public Animator Ani_anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ani_anim.SetTrigger("isStart");
+
+    }
+
+    public void OnClickText()
+    {
+        Ani_anim.SetTrigger("isFinish");
+        //BTN_SSH_Text.gameObject.SetActive(false);
+    }
+}
