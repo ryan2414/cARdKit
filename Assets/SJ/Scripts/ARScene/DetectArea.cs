0c761f173512006e12ba3e1c3c4fc2e08dd311df
ryan2414 <75060346+ryan2414@users.noreply.github.com>
2021-05-04 14:44:55 +0900

    AR Scene 회로 프리팹 이미지 변경/ 소자 감지 스크립트 변경/소자 마커 콜라이더 제거

--- Assets/SJ/Scripts/ARScene/DetectArea.cs
+++ Assets/SJ/Scripts/ARScene/DetectArea.cs
@@ -1,17 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
+//특정 포인트를 기준으로 그 안에 특정 오브젝트가 들어면
+//트루를 표시한다.
 public class DetectArea : MonoBehaviour
 {
-    //인식 당하는거
+    //종이 마커 위에 있는 오브젝트
     public GameObject interactionGameObject;
     public Vector2 interactionGameObjectPoint;
 
-    //뽑아 내는거
+    //종이 위에 있는 오브젝트의 인아웃 여부를 확인하는 포인트
     public GameObject[] targetPoints;
     public Vector2[] targetPolygons;
 
     [SerializeField]
     bool isIn;
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +28,7 @@ public class DetectArea : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         for (int i = 0; i < targetPoints.Length; i++)
         {
             targetPolygons[i] = Camera.main.WorldToScreenPoint(targetPoints[i].transform.position);
@@ -29,5 +36,67 @@ public class DetectArea : MonoBehaviour
         interactionGameObjectPoint = Camera.main.WorldToScreenPoint(interactionGameObject.transform.position);
 
         isIn = PolyUtil.IsPointInPolygon(interactionGameObjectPoint, targetPolygons);
+
+        ActiveObj(isIn);
+    }
+
+    //회로 위에 실행시킬 오브젝트
+    public GameObject electricObj;
+
+    bool objectActive;
+    //isIn이 트루이면 게임 오브젝트를 트루로 바꿔준다.
+    void ActiveObj(bool isIn)
+    {
+        if (isIn == true)
+        {
+            electricObj.SetActive(true);//회로 위의 소자 활성화
+            interactionGameObject.SetActive(false);//종이 마커 위의 소자 끄기
+            transform.GetComponentInChildren<MeshRenderer>().enabled = false;//회로 마커 비 활성화
+            objectActive = true;
+            //StopCoroutine(FalsePrefab());
+            //StartCoroutine(ActivePrefab());
+        }
+        else
+        {
+            if (objectActive)
+            {
+                //isIn이 false면  오브젝트가 사라진다.
+
+                electricObj.SetActive(false);//회로 위의 소자 비활성화
+                interactionGameObject.SetActive(true);//종이 마커 위의 소자 켜기
+                transform.GetComponentInChildren<MeshRenderer>().enabled = true;//회로 마커 활성화
+            }
+            
+            //StopCoroutine(ActivePrefab());
+            //StartCoroutine(FalsePrefab());
+        }
+    }
+
+    //3초 동안 오브젝트가 위치에 있으면 오브젝트를 활성화 한다.
+    IEnumerator ActivePrefab()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (i >= 2)
+            {
+                electricObj.SetActive(true);//회로 위의 소자 활성화
+                interactionGameObject.SetActive(false);//종이 마커 위의 소자 끄기
+                transform.GetComponentInChildren<MeshRenderer>().enabled = false;//회로 마커 비 활성화
+            }
+            yield return new WaitForSeconds(1f);
+        }
+    }
+    IEnumerator FalsePrefab()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (i >= 2)
+            {
+                electricObj.SetActive(false);//회로 위의 소자 비활성화
+                interactionGameObject.SetActive(true);//종이 마커 위의 소자 켜기
+                transform.GetComponentInChildren<MeshRenderer>().enabled = true;//회로 마커 활성화
+            }
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
