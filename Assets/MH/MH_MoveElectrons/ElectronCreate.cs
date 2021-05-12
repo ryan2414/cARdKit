e621f8ecb628b93c96f70b5b2f3fa1b7b7b33b9a
Moon-Line <74943892+Moon-Line@users.noreply.github.com>
2021-05-12 18:07:21 +0900

    전자 등등, 물체 이동 스크립트 및 폴더

---
+++ Assets/MH/MH_MoveElectrons/ElectronCreate.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectronCreate : MonoBehaviour
+{
+    public GameObject electronPrefab;
+
+    public List<Transform> movePoints = new List<Transform>();
+    //public List<GameObject> electrons = new List<GameObject>();
+
+    [SerializeField]
+    float timer = 3f;
+    float currentTime;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime > timer)
+        {
+            GameObject tempGo = Instantiate(electronPrefab);
+            tempGo.transform.position = movePoints[0].position;
+            tempGo.transform.parent = gameObject.transform;
+            tempGo?.GetComponent<ElectronMove>().GetDepart(movePoints);
+
+            //electrons.Add(tempGo);
+            currentTime = 0;
+        }
+    }
+}
---
+++ Assets/MH/MH_MoveElectrons/ElectronMove.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectronMove : MonoBehaviour
+{
+    List<Transform> departments = new List<Transform>();
+    int index_Department;
+
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        index_Department = 0;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    public void GetDepart(List<Transform> movePoints)
+    {
+        departments = movePoints;
+        index_Department++;
+    }
+
+    void Move()
+    {
+        if (departments != null)
+        {
+
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, departments[index_Department].position, moveSpeed * Time.deltaTime);
+            gameObject.transform.LookAt(departments[index_Department].position);
+
+            if (Vector3.Distance(gameObject.transform.position, departments[index_Department].position) < 0.001f)
+            {
+                index_Department++;
+                if (index_Department > (departments.Count - 1))
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+
+}
