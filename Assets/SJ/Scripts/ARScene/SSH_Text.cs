393ab6e400d393e25b8b39ab26b7a1cf9111557b
ryan2414 <75060346+ryan2414@users.noreply.github.com>
2021-05-26 14:34:42 +0900

    스테이지에 따른 다사 변경 && 도움말 버튼 추가

--- Assets/SJ/Scripts/ARScene/SSH_Text.cs
+++ Assets/SJ/Scripts/ARScene/SSH_Text.cs
@@ -8,7 +8,56 @@ public class SSH_Text : MonoBehaviour
     public Animator Ani_anim;
     public AudioSource soundWhip;
     bool isPlayed;
+    bool isStartAnim;
 
+    public Text Sh_QstTxt;
+
+    private void Start()
+    {
+        int stageNum = FlagManager.instance.stageNum;
+
+        switch (stageNum)
+        {
+            case 1:
+                Sh_QstTxt.text = "카드를 가져다 올바른 위치에 놓자!";
+
+                    break;
+            case 2:
+                Sh_QstTxt.text = "1. 카드를 가져다 올바른 위치에 놓자!\n2.화면의 스위치를 터치해서 회로를 연결하자!";
+                    break;
+            case 3:
+                Sh_QstTxt.text = "1. 카드를 가져다 올바른 위치에 놓자!\n2.화면의 스위치를 터치해서 회로를 연결하자!\n3.가변저항의 손잡이를 움직여 불을 더 밝게해 보자!";
+                    break;
+            case 5:
+                Sh_QstTxt.text = "1. 카드를 가져다 올바른 위치에 놓자!\n2.화면의 스위치를 터치해서 회로를 연결하자!";
+                break;
+            case 6:
+                Sh_QstTxt.text = "1. 카드를 가져다 올바른 위치에 놓자!\n2.화면의 스위치를 터치해서 회로를 연결하자!\n3.가변저항의 손잡이를 움직여 선풍기를 더 빠르게 해보자";
+                break;
+
+            default:
+                break;
+        }
+    }
+
+
+    public void OnClickActQst()
+    {
+        if (isPlayed) {
+            Ani_anim.SetTrigger("isStart");
+            isPlayed = false;
+        }
+        
+    }
+    public void FinishAnim()
+    {
+        if (!isStartAnim)
+        {
+            Ani_anim.SetTrigger("isFinish");
+            isStartAnim = true;
+            isPlayed = true;
+        }
+    }
     public void OnClickText()
     {
         if (!isPlayed)
